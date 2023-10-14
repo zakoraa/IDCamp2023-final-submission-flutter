7c1c0459f83b9428259376172b4d9cc08b51f75e
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-10-14 11:43:04 +0700

    add additional attribute to model

--- lib/data/jkt48_member_data.dart
+++ lib/data/jkt48_member_data.dart
@@ -6,39 +6,46 @@ List<JKT48Member> listMember = [
       dateOfBirth: "8 Januari 2005",
       bloodGroup: "B",
       height: 160,
+      gen: 9,
       profilePicture:
           "https://jkt48.com/profile/adzana_shaliha.jpg?v=20230116"),
   JKT48Member(
       name: "Amanda Sukma",
       dateOfBirth: "17 Desember 2004",
       height: 160,
+       gen: 10,
       profilePicture: "https://jkt48.com/profile/amanda_sukma.jpg?v=20230530"),
   JKT48Member(
       name: "Angelina Christy",
       dateOfBirth: "5 Desember 2005",
       height: 163,
+       gen: 7,
       profilePicture:
           "https://jkt48.com/profile/angelina_christy.jpg?v=20230116"),
   JKT48Member(
       name: "Aurellia",
       dateOfBirth: "29 Oktober 2002",
       height: 155,
+       gen: 10,
       profilePicture: "https://jkt48.com/profile/aurellia.jpg?v=20230531"),
   JKT48Member(
       name: "Azizi Asadel",
       dateOfBirth: "16 Mei 2004",
       height: 162,
+       gen: 7,
       profilePicture: "https://jkt48.com/profile/azizi_asadel.jpg?v=20230116"),
   JKT48Member(
       name: "Callista Alifia",
       dateOfBirth: "8 Agustus 2005",
       height: 157,
+       gen: 10,
       profilePicture:
           "https://jkt48.com/profile/callista_alifia.jpg?v=20230531"),
   JKT48Member(
       name: "Cornelia Vanisa",
       dateOfBirth: "26 Juli 2002",
       height: 162,
+      gen: 8,
       profilePicture:
           "https://jkt48.com/profile/cornelia_vanisa.jpg?v=20230116"),
   JKT48Member(
@@ -46,30 +53,35 @@ List<JKT48Member> listMember = [
       dateOfBirth: "26 Februari 2005",
       bloodGroup: "B",
       height: 154,
+      gen: 7,
       profilePicture:
           "https://jkt48.com/profile/febriola_sinambela.jpg?v=20230116"),
   JKT48Member(
       name: "Feni Fitriyanti",
       dateOfBirth: "16 Januari 1999",
       height: 162,
+      gen: 3,
       profilePicture:
           "https://jkt48.com/profile/feni_fitriyanti.jpg?v=20230116"),
   JKT48Member(
       name: "Fiony Alveria",
       dateOfBirth: "4 Februari 2002",
       height: 158,
+      gen: 8,
       profilePicture: "https://jkt48.com/profile/fiony_alveria.jpg?v=20230116"),
   JKT48Member(
       name: "Flora Shafiqa Riyadi",
       dateOfBirth: "4 April 2005",
       bloodGroup: "B",
       height: 149,
+      gen: 8,
       profilePicture: "https://jkt48.com/profile/flora_shafiq.jpg?v=20230116"),
   JKT48Member(
       name: "Freya Jayawardana",
       dateOfBirth: "13 Februari 2006",
       bloodGroup: "B",
       height: 154,
+      gen: 7,
       profilePicture:
           "https://jkt48.com/profile/freya_jayawardana.jpg?v=20230116"),
   JKT48Member(
@@ -77,12 +89,14 @@ List<JKT48Member> listMember = [
       dateOfBirth: "7 Agustus 2006",
       bloodGroup: "B",
       height: 160,
+      gen: 7,
       profilePicture:
           "https://jkt48.com/profile/gabriela_abigail.jpg?v=20230531"),
   JKT48Member(
       name: "Gita Sekar Andarini",
       dateOfBirth: "30 Juni 2001",
       height: 165,
+      gen: 6,
       profilePicture:
           "https://jkt48.com/profile/gita_sekar_andarini.jpg?v=20230116"),
   JKT48Member(
@@ -90,35 +104,41 @@ List<JKT48Member> listMember = [
       dateOfBirth: "15 Juni 2000",
       bloodGroup: "A",
       height: 165,
+      gen: 7,
       profilePicture: "https://jkt48.com/profile/helisma_putri.jpg?v=20230116"),
   JKT48Member(
       name: "Indah Cahya",
       dateOfBirth: "20 Maret 2001",
       bloodGroup: "A",
       height: 165,
+      gen: 9,
       profilePicture: "https://jkt48.com/profile/indah_cahya.jpg?v=20230116"),
   JKT48Member(
       name: "Indira Seruni",
       dateOfBirth: "26 April 2004",
       bloodGroup: "A",
       height: 160,
+      gen: 10,
       profilePicture: "https://jkt48.com/profile/indira_seruni.jpg?v=20230531"),
   JKT48Member(
       name: "Jessica Chandra",
       dateOfBirth: "23 September 2005",
       height: 163,
+      gen: 7,
       profilePicture:
           "https://jkt48.com/profile/jessica_chandra.jpg?v=20230116"),
   JKT48Member(
       name: "Jesslyn Elly",
       dateOfBirth: "13 September 2001",
       height: 155,
+      gen: 10,
       profilePicture: "https://jkt48.com/profile/jesslyn_elly.jpg?v=20230531"),
   JKT48Member(
       name: "Kathrina Irene",
       dateOfBirth: "26 Juli 2006",
       bloodGroup: "A",
       height: 165,
+      gen: 9,
       profilePicture:
           "https://jkt48.com/profile/kathrina_irene.jpg?v=20230116"),
   JKT48Member(
@@ -126,12 +146,14 @@ List<JKT48Member> listMember = [
       dateOfBirth: "23 Oktober 2002",
       bloodGroup: "B",
       height: 157,
+      gen: 8,
       profilePicture:
           "https://jkt48.com/profile/lulu_salsabila.jpg?v=20230116"),
   JKT48Member(
       name: "Marsha Lenathea",
       dateOfBirth: "9 Januari 2006",
       height: 163,
+      gen: 9,
       profilePicture:
           "https://jkt48.com/profile/marsha_lenathea.jpg?v=20230116"),
   JKT48Member(
@@ -139,6 +161,7 @@ List<JKT48Member> listMember = [
       dateOfBirth: "12 Juli 2004",
       bloodGroup: "B",
       height: 158,
+      gen: 7,
       profilePicture:
           "https://jkt48.com/profile/mutiara_azzahra.jpg?v=20230116"),
   JKT48Member(
@@ -146,18 +169,21 @@ List<JKT48Member> listMember = [
       dateOfBirth: "11 November 2007",
       bloodGroup: "AB",
       height: 163,
+      gen: 10,
       profilePicture: "https://jkt48.com/profile/raisha_syifa.jpg?v=20230530"),
   JKT48Member(
       name: "Reva Fidela",
       dateOfBirth: "14 Juli 2006",
       bloodGroup: "A",
       height: 159,
+      gen: 8,
       profilePicture: "https://jkt48.com/profile/reva_fidela.jpg?v=20230116"),
   JKT48Member(
       name: "Shani Indira Natio",
       dateOfBirth: "5 Oktober 1998",
       bloodGroup: "B",
       height: 167,
+      gen: 3,
       profilePicture:
           "https://jkt48.com/profile/shani_indira_natio.jpg?v=20230116"),
   JKT48Member(
@@ -165,12 +191,14 @@ List<JKT48Member> listMember = [
       dateOfBirth: "31 Agustus 1999",
       bloodGroup: "A",
       height: 161,
+      gen: 3,
       profilePicture: "https://jkt48.com/profile/shania_gracia.jpg?v=20230116"),
   JKT48Member(
       name: "Yessica Tamara",
       dateOfBirth: "24 September 2002",
       bloodGroup: "A",
       height: 165,
+      gen: 7,
       profilePicture:
           "https://jkt48.com/profile/yessica_tamara.jpg?v=20230116"),
 ];
--- lib/model/jkt48_member_model.dart
+++ lib/model/jkt48_member_model.dart
@@ -1,12 +1,13 @@
 class JKT48Member {
   String name, dateOfBirth, bloodGroup, profilePicture;
-  int height;
+  int height, gen;
 
   JKT48Member({
     required this.name,
     required this.dateOfBirth,
     this.bloodGroup = "O",
     required this.height,
+    required this.gen,
     required this.profilePicture,
   });
 }
