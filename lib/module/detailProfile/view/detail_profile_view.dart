0ebce70b6afe62ddda2404212c4c035b9d462446
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-10-14 14:00:42 +0700

    style: update detail profile page view

--- lib/module/detailProfile/view/detail_profile_view.dart
+++ lib/module/detailProfile/view/detail_profile_view.dart
@@ -20,82 +20,98 @@ class DetailProfileView extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: const Text("Anggota JKT48"),
+          actions: [
+            jkt48Member.name == "Flora Shafiqa Riyadi"
+                ? const Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Icon(
+                      Icons.star,
+                      color: Color.fromARGB(255, 255, 251, 7),
+                    ),
+                  )
+                : const SizedBox.shrink()
+          ],
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    border: Border.all(width: 1, color: Colors.grey.shade300)),
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(5),
-                    child: Image.network(jkt48Member.profilePicture)),
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border:
+                          Border.all(width: 1, color: Colors.grey.shade300)),
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(5),
+                      child: Image.network(jkt48Member.profilePicture)),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            Column(
-              children: List.generate(data.length, (index) {
-                final text = data.keys.elementAt(index);
-                final dataMember = data.values.elementAt(index);
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Container(
-                          margin: const EdgeInsets.symmetric(vertical: 10),
-                          width: MediaQuery.of(context).size.width * 0.75,
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(text,
-                                  style: textTheme.bodyMedium!
-                                      .copyWith(fontSize: 16)),
-                              text == "Gen"
-                                  ? Container(
-                                      height: 30,
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 10),
-                                      decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(5),
-                                          color: CustomColor.primaryColor),
-                                      child: Center(
-                                        child: Text(
-                                          "Gen $dataMember",
-                                          style: textTheme.bodyMedium!.copyWith(
-                                              color:
-                                                  CustomColor.secondaryColor),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Column(
+                children: List.generate(data.length, (index) {
+                  final key = data.keys.elementAt(index);
+                  final value = data.values.elementAt(index);
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(vertical: 10),
+                            padding: const EdgeInsets.symmetric(horizontal: 5),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(key,
+                                    style: textTheme.bodyMedium!
+                                        .copyWith(fontSize: 16)),
+                                key == "Gen"
+                                    ? Container(
+                                        height: 30,
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 10),
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(5),
+                                            color: CustomColor.primaryColor),
+                                        child: Center(
+                                          child: Text(
+                                            "Gen $value",
+                                            style: textTheme.bodyMedium!
+                                                .copyWith(
+                                                    color: CustomColor
+                                                        .secondaryColor),
+                                          ),
                                         ),
-                                      ),
-                                    )
-                                  : Text(
-                                      "$dataMember",
-                                      textAlign: TextAlign.start,
-                                      style: textTheme.bodySmall!
-                                          .copyWith(fontSize: 14),
-                                    )
-                            ],
+                                      )
+                                    : Text(
+                                        "$value",
+                                        textAlign: TextAlign.start,
+                                        style: textTheme.bodySmall!
+                                            .copyWith(fontSize: 14),
+                                      )
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: 1,
-                        color: Colors.grey.shade300,
-                      )
-                    ],
-                  ),
-                );
-              }),
-            )
-          ],
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 1,
+                          color: Colors.grey.shade300,
+                        )
+                      ],
+                    ),
+                  );
+                }),
+              )
+            ],
+          ),
         ));
   }
 }
